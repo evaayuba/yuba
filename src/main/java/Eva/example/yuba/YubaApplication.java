fada44e27a01553dccdd03902bdd495c89ab7cd8
evaayuba <yubaeva@29gmail.com>
2023-07-03 03:32:20 -0600

    yuba commit

---
+++ src/main/java/Eva/example/yuba/YubaApplication.java
@@ -0,0 +1,13 @@
+package Eva.example.yuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YubaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YubaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Eva/example/yuba/YubaApplicationTests.java
@@ -0,0 +1,13 @@
+package Eva.example.yuba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YubaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
